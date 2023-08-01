9acf8d529b37ca29a91b45e49dab905be7dd320f
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-01 20:04:55 +0300

    Update HomeViewController.swift
    for collections lists

--- MovieDB/MovieDB/Scene/Home/HomeViewController.swift
+++ MovieDB/MovieDB/Scene/Home/HomeViewController.swift
@@ -9,8 +9,33 @@ import UIKit
 
 class HomeViewController: BaseViewController<HomeViewModel> {
     @IBOutlet weak var movieCollectionListTableView: UITableView!
-
+    
+    var collectionModels = [MovieModelProtocol]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        movieCollectionListTableView.delegate = self
+        movieCollectionListTableView.dataSource = self
+        
+        let tableViewCell = String(describing: MovieCollectionTableViewCell.self)
+        movieCollectionListTableView.register(UINib(nibName: tableViewCell, bundle: nil), forCellReuseIdentifier: tableViewCell)
+    }
+}
+
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell = String(describing: MovieCollectionTableViewCell.self)
+        let cell = movieCollectionListTableView.dequeueReusableCell(withIdentifier: tableViewCell, for: indexPath) as! MovieCollectionTableViewCell
+        cell.collectionTitle.text = "trial tableView cell"
+        return cell
     }
 }
