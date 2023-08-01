662e3d4d9efaa075d8e1e2bb5c65fbea9fb1bfc7
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-01 16:58:15 +0300

    MovieListCollectionView for MovieCollectionTableViewCell created

---
+++ MovieDB/MovieDB/Scene/Cells/MovieListCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MovieListCollectionViewCell.swift
+//  MovieDB
+//
+//  Created by MacBook on 1.08.2023.
+//
+
+import UIKit
+
+class MovieListCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var posterIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var movieName: UILabel!
+    @IBOutlet weak var movieDate: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+}
