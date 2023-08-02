743bb88d1b331815291862897944228b51c05d60
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-02 12:28:27 +0300

    fetched movie data from network, transferred to tableViewCell

--- MovieDB/MovieDB/Scene/Home/HomeViewController.swift
+++ MovieDB/MovieDB/Scene/Home/HomeViewController.swift
@@ -14,7 +14,9 @@ class HomeViewController: BaseViewController<HomeViewModel> {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel = HomeViewModel()
         configureTableView()
+        getCollectionMovies()
     }
     
     private func configureTableView() {
@@ -24,18 +26,43 @@ class HomeViewController: BaseViewController<HomeViewModel> {
         let tableViewCell = String(describing: MovieCollectionTableViewCell.self)
         movieCollectionListTableView.register(UINib(nibName: tableViewCell, bundle: nil), forCellReuseIdentifier: tableViewCell)
     }
+    
+    private func getCollectionMovies() {
+        viewModel?.getTrendMovies() { [weak self] (data) in
+            guard let self else {return}
+            if var result = data {
+                result.collectionTitle = "Trend Movies"
+                self.collectionModels.append(result)
+                self.movieCollectionListTableView.reloadData()
+            } else {
+                print("NOT GET TREND MOVİES")
+            }
+        }
+        
+        viewModel?.getPopularMovies(){ [weak self] (data) in
+            guard let self else {return}
+            if var result = data {
+                result.collectionTitle = "Popular Movies"
+                self.collectionModels.append(result)
+                self.movieCollectionListTableView.reloadData()
+            } else {
+                print("NOT GET POPULAR MOVİES")
+            }
+        }
+    }
 }
 
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return collectionModels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let tableViewCell = String(describing: MovieCollectionTableViewCell.self)
         let cell = movieCollectionListTableView.dequeueReusableCell(withIdentifier: tableViewCell, for: indexPath) as! MovieCollectionTableViewCell
-        cell.collectionTitle.text = "trial tableView cell"
+        cell.collectionTitle.text = collectionModels[indexPath.row].collectionTitle
+        cell.model = collectionModels[indexPath.row].results
         return cell
     }
 }
