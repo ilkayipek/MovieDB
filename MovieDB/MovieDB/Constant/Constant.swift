3bef0fbde32f62aa01a3dd9084f845a0ea78f68b
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-02 12:24:02 +0300

    fix Constant

--- MovieDB/MovieDB/Constant/Constant.swift
+++ MovieDB/MovieDB/Constant/Constant.swift
@@ -5,10 +5,11 @@
 //  Created by MacBook on 1.08.2023.
 //
 
-import UIKit.UIViewController
+import Foundation
 
 final class Constant {
     static let shared: Constant = Constant()
-    let apiKey: String = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as! String
-    let baseUrl: String = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as! String
+    let apiKey: String = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as! String
+    let baseUrl: String = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as! String
 }
+
