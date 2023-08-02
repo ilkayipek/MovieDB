3b513dad142fa5291cb0c80e3ba1faa749e75aa7
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-02 13:13:15 +0300

    image constants added in constant+RequestPath

--- MovieDB/MovieDB/Constant/Constant+RequestParthMovie.swift
+++ MovieDB/MovieDB/Constant/Constant+RequestParthMovie.swift
@@ -5,16 +5,27 @@
 //  Created by MacBook on 1.08.2023.
 //
 
+import Foundation
+
 extension Constant {
     enum RequestPathMovie: String {
         case popularMovie = "/3/movie/popular?api_key="
         case trendDayMovie = "/3/trending/movie/day?api_key="
+        case imageBaseUrlPath = "https://image.tmdb.org/t/p/"
         
         static func popularMoviesPath() -> String {
             return "\(popularMovie.rawValue)\(Constant.shared.apiKey)"
         }
+        
         static func trendDayMoviesPath() -> String {
             return "\(trendDayMovie.rawValue)\(Constant.shared.apiKey)"
+        }
+        
+        static func imageUrl(imageSize: ImageSizeSections, path: String?) -> URL? {
+            guard let path else { return nil }
+            if let url = URL(string: "\(imageBaseUrlPath.rawValue)\(imageSize.rawValue)\(path)") {
+                return url
+            } else { return nil }
         }
     }
 }
