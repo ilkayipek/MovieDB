868e63b6f23d61de7c565005ed51ec8e387d9b7e
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-15 19:20:31 +0300

    on-screen features added

--- MovieDB/MovieDB/Scene/DetailMovie/DetailMovieViewController.swift
+++ MovieDB/MovieDB/Scene/DetailMovie/DetailMovieViewController.swift
@@ -8,6 +8,25 @@
 import UIKit
 
 class DetailMovieViewController: BaseViewController<DetailMovieViewModel> {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var loadSpinner: UIActivityIndicatorView!
+    @IBOutlet weak var backgroundPoster: UIImageView!
+    @IBOutlet weak var overViewTextView: UITextView!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var watchListButton: UIButton!
+    @IBOutlet weak var starButton: UIButton!
+    @IBOutlet weak var runtimeLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var tagLineLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var movieName: UILabel!
+    @IBOutlet weak var genreExpampleLabel: UILabel!
+    @IBOutlet weak var genreView: UIView!
+    @IBOutlet weak var genreViewyConstraint: NSLayoutConstraint!
+    @IBOutlet weak var overViewContainerHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var scoreIndicatorView: ScoreIndicatorView!
+    @IBOutlet weak var voteCountLabel: UILabel!
+    @IBOutlet weak var overViewArrowButon: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
