b9aefa82d4bf900b957fb9a512ac2fdc0adf90bc
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-16 17:53:43 +0300

    Create CustomUILabel.swift

---
+++ MovieDB/MovieDB/Scene/CustomViews/CustomUILabel.swift
@@ -0,0 +1,54 @@
+//
+//  CustomUILabel.swift
+//  MovieDB
+//
+//  Created by MacBook on 16.08.2023.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomUILabel: UILabel {
+    @IBInspectable var cornerRadiusValue: CGFloat = 10.0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 1.0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = .black {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpView()
+    }
+
+    private func setUpView() {
+        layer.cornerRadius = cornerRadiusValue
+        layer.borderWidth = borderWidth
+        layer.borderColor = borderColor.cgColor
+        clipsToBounds = true
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        layer.cornerRadius = cornerRadiusValue
+        layer.borderWidth = borderWidth
+        layer.borderColor = borderColor.cgColor
+    }
+}
