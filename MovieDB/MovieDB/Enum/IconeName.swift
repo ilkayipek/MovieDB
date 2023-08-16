1588b936653e67719d1c460950fb6370c789df62
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-16 21:54:39 +0300

    Create IconeName.swift

---
+++ MovieDB/MovieDB/Enum/IconeName.swift
@@ -0,0 +1,13 @@
+//
+//  IconeName.swift
+//  MovieDB
+//
+//  Created by MacBook on 16.08.2023.
+//
+
+enum IconeName: String {
+    case calendar
+    case clock
+    case arrowChevronDown = "chevron.down"
+    case arrowChevronUp = "chevron.up"
+}
