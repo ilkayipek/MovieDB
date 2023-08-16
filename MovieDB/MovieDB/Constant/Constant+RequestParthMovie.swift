c1a316c957ba76d15d5edcf3b1b3ab532c7796af
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-16 17:54:30 +0300

    Update Constant+RequestParthMovie.swift
    DetailMoviePath added

--- MovieDB/MovieDB/Constant/Constant+RequestParthMovie.swift
+++ MovieDB/MovieDB/Constant/Constant+RequestParthMovie.swift
@@ -12,6 +12,10 @@ extension Constant {
         case popularMovie = "/3/movie/popular?api_key="
         case trendDayMovie = "/3/trending/movie/day?api_key="
         case imageBaseUrlPath = "https://image.tmdb.org/t/p/"
+        case movieDetail = "/3/movie/"
+        case translationMaviePath = "/3/movie"
+        case languagePath = "&language="
+        case apiKeyPath = "?api_key="
         
         static func popularMoviesPath() -> String {
             return "\(popularMovie.rawValue)\(Constant.shared.apiKey)"
@@ -26,6 +30,15 @@ extension Constant {
             if let url = URL(string: "\(imageBaseUrlPath.rawValue)\(imageSize.rawValue)\(path)") {
                 return url
             } else { return nil }
+        }
+        
+        static func detailOriginalMoviePath(movieId: Int) -> String {
+            return "\(movieDetail.rawValue)\(movieId)\(apiKeyPath.rawValue)\(Constant.shared.apiKey)"
+        }
+        
+        static func translationMoviePath(movieId: Int, language: Language) -> String {
+            let apiKey = Constant.shared.apiKey
+            return "\(translationMaviePath.rawValue)/\(movieId)\(apiKeyPath.rawValue)\(apiKey)\(languagePath.rawValue)\(language.rawValue)"
         }
     }
 }
