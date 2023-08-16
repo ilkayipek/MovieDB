75260224d9e488343fc3fcfeb26dff8007d21c1e
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-17 00:08:38 +0300

    DetailMovieTableViewCell xib created

---
+++ MovieDB/MovieDB/Scene/Cells/DetailMovieTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  DetailMovieTableViewCell.swift
+//  MovieDB
+//
+//  Created by MacBook on 17.08.2023.
+//
+
+import UIKit
+
+class DetailMovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+}
