9be175fe8809f16194c72be540e4cba97d0f4264
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-16 23:32:57 +0300

    ActorsCollectionViewCell xib file created

---
+++ MovieDB/MovieDB/Scene/Cells/ActorsCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ActorsCollectionViewCell.swift
+//  MovieDB
+//
+//  Created by MacBook on 16.08.2023.
+//
+
+import UIKit
+
+class ActorsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
