e1f61f704b0ce5a348e8ed9085d8b8f7f21ba1bd
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-16 18:02:39 +0300

    Update DetailMovieViewModel.swift
    data received from network

--- MovieDB/MovieDB/Scene/DetailMovie/DetailMovieViewModel.swift
+++ MovieDB/MovieDB/Scene/DetailMovie/DetailMovieViewModel.swift
@@ -8,5 +8,37 @@
 import Foundation
 
 class DetailMovieViewModel: BaseViewModel {
+    func getMovieDetailTranslation(movieId: Int, closure: @escaping(DetailMovieModel?) -> Void) -> Void {
+        let path = Constant.RequestPathMovie.translationMoviePath(movieId: movieId, language: .tr)
+        Network.shared.getRequestV3(urlPath: path) { (result: Result<DetailMovieModel?, Error>) in
+            switch result {
+            case .success(let data):
+                if data?.success ?? true {
+                    closure(data)
+                } else {
+                    closure(nil)
+                }
+            case .failure(let error):
+                print("ERROR MESSAGE:\(error.localizedDescription)")
+                closure(nil)
+            }
+        }
+    }
     
+    func getMovieDetailOriginal(movieId: Int, closure: @escaping(DetailMovieModel?) -> Void) -> Void {
+        let path = Constant.RequestPathMovie.detailOriginalMoviePath(movieId: movieId)
+        Network.shared.getRequestV3(urlPath: path) { (result: Result<DetailMovieModel, Error>) in
+            switch result {
+            case .success(let data):
+                if data.success ?? true {
+                    closure(data)
+                } else {
+                    closure(nil)
+                }
+            case .failure(let error):
+                print("ERROR MESSAGE:\(error.localizedDescription)")
+                closure(nil)
+            }
+        }
+    }
 }
