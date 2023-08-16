76b7fcc66ab018c657c82864b49d5ea78abbddab
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-16 23:33:42 +0300

    TrailerCollectionViewCell xib created

---
+++ MovieDB/MovieDB/Scene/Cells/TrailerCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TrailerCollectionViewCell.swift
+//  MovieDB
+//
+//  Created by MacBook on 16.08.2023.
+//
+
+import UIKit
+
+class TrailerCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+}
