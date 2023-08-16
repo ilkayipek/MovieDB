1aff73069f406dc0f78aa84776fe2828eb49d760
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-16 17:53:34 +0300

    Create CustomUIButton.swift

---
+++ MovieDB/MovieDB/Scene/CustomViews/CustomUIButton.swift
@@ -0,0 +1,55 @@
+//
+//  CustomUIButton.swift
+//  MovieDB
+//
+//  Created by MacBook on 16.08.2023.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomUIButton: UIButton {
+    @IBInspectable var cornerRadiusValue: CGFloat = 10.0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 1.0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = .black {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpView()
+    }
+
+    private func setUpView() {
+        layer.cornerRadius = cornerRadiusValue
+        layer.borderWidth = borderWidth
+        layer.borderColor = borderColor.cgColor
+        clipsToBounds = true
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        layer.cornerRadius = cornerRadiusValue
+        layer.borderWidth = borderWidth
+        layer.borderColor = borderColor.cgColor
+    }
+}
+
