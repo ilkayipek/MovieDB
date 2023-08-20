543208678eece39805c9ec1265136ebab32b4af9
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-20 22:10:46 +0300

    Create DetailMovieCollectionModelProtocol.swift
    the protocol created for different model to connect

---
+++ MovieDB/MovieDB/Protocol/DetailMovieCollectionModelProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  DetailMovieCollectionModelProtocol.swift
+//  MovieDB
+//
+//  Created by MacBook on 20.08.2023.
+//
+
+protocol DetailMovieCollectionModelProtocol {
+    var collectionTitle: String? { get set }
+}
