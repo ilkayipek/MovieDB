71b9b574844d308761a79c923fdb77622f9bfeea
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-20 22:09:01 +0300

    Create MovieTrailerResultModel.swift

---
+++ MovieDB/MovieDB/Models/Movie/Detail/MovieTrailerResultModel.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTrailerResultModel.swift
+//  MovieDB
+//
+//  Created by MacBook on 20.08.2023.
+//
+
+// MARK: - Result
+struct MovieTrailerResultModel: Decodable {
+    let iso639_1, iso3166_1, name, key: String?
+    let site: String?
+    let size: Int?
+    let type: String?
+    let official: Bool?
+    let publishedAt, id: String?
+
+    enum CodingKeys: String, CodingKey {
+        case iso639_1 = "iso_639_1"
+        case iso3166_1 = "iso_3166_1"
+        case name, key, site, size, type, official
+        case publishedAt = "published_at"
+        case id
+    }
+}
