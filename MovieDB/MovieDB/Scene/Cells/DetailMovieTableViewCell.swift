80a0f423464931e735f9425033b3dc7a6228a1d0
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-20 22:00:33 +0300

    Update DetailMovieTableViewCell.swift
    Trailer view is listed in CollectionView in TableView for trial

--- MovieDB/MovieDB/Scene/Cells/DetailMovieTableViewCell.swift
+++ MovieDB/MovieDB/Scene/Cells/DetailMovieTableViewCell.swift
@@ -13,6 +13,8 @@ class DetailMovieTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        configureCollectionView()
+        collectionView.reloadData()
         
     }
 
@@ -21,4 +23,32 @@ class DetailMovieTableViewCell: UITableViewCell {
         
     }
     
+    private func configureCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        let actors = String(describing: ActorsCollectionViewCell.self)
+        collectionView.register(UINib(nibName: actors, bundle: nil), forCellWithReuseIdentifier: actors)
+        
+        let trailer = String(describing: TrailerCollectionViewCell.self)
+        collectionView.register(UINib(nibName: trailer, bundle: nil), forCellWithReuseIdentifier: trailer)
+    }
+    
 }
+
+extension DetailMovieTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: TrailerCollectionViewCell.self), for: indexPath) as! TrailerCollectionViewCell
+       
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: 225, height: 125)
+    }
+}
+
