b56c7a6e8fbe03d329cb611c20ad770d95b70572
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-22 15:47:18 +0300

    Create AuthV4Headers.swift
    created for use in network class

---
+++ MovieDB/MovieDB/Enum/AuthV4Headers.swift
@@ -0,0 +1,11 @@
+//
+//  AuthV4Headers.swift
+//  MovieDB
+//
+//  Created by MacBook on 22.08.2023.
+//
+
+enum AuthV4Headers: String {
+    case contentType = "Content-Type"
+    case authorization = "Authorization"
+}
