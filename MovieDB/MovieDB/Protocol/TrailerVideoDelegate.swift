fe6f4a516d9fde85023bfaf2eefa17d6f746152b
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-23 21:58:29 +0300

    Create TrailerVideoDelegate.swift
    created to listen to selected trailer video by Detail Movie View Controller

---
+++ MovieDB/MovieDB/Protocol/TrailerVideoDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  TrailerVideoDelegate.swift
+//  MovieDB
+//
+//  Created by MacBook on 23.08.2023.
+//
+
+import Foundation
+
+protocol TrailerVideoDelegate {
+    func selectedVideo(videoKey: String)
+}
