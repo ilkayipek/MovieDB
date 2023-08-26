1b7838176ad55b958779eef9f1130eea496b6738
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-26 20:41:21 +0300

    created DetailMovieContentTableViewCell
    Content Segmentation will be done for the Detail Movie scene.

---
+++ MovieDB/MovieDB/Scene/Cells/DetailMovie/DetailMovieContentTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  DetailMovieContentTableViewCell.swift
+//  MovieDB
+//
+//  Created by MacBook on 26.08.2023.
+//
+
+import UIKit
+
+class DetailMovieContentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+}
