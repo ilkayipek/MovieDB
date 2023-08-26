c348aaaa2f75c18ae8e404f3fbde2c2cd82af280
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-26 20:41:50 +0300

    created DetailMovieFooterTableViewCell
    Footer Segmentation will be done for the Detail Movie scene.

---
+++ MovieDB/MovieDB/Scene/Cells/DetailMovie/DetailMovieFooterTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  DetailMovieFooterTableViewCell.swift
+//  MovieDB
+//
+//  Created by MacBook on 26.08.2023.
+//
+
+import UIKit
+
+class DetailMovieFooterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
