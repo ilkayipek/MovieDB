39127d1652935c575c5e2b036a3b4776222bcda3
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-30 00:34:38 +0300

    Create SimilarMovieModel.swift

---
+++ MovieDB/MovieDB/Models/Movie/SimilarMovieModel.swift
@@ -0,0 +1,20 @@
+//
+//  SimilarMovieModel.swift
+//  MovieDB
+//
+//  Created by MacBook on 29.08.2023.
+//
+
+// MARK: - SimilarMovieModel
+struct SimilarMovieModel: Decodable, DetailMovieCollectionModelProtocol {
+    var collectionTitle: String?
+    let page: Int?
+    let results: [MovieResult]?
+    let totalPages, totalResults: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
