ae9eba9ed824b485383c9c2077913e938f5e4a4d
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-08-29 21:24:22 +0300

    Update DetailMovieViewModel.swift
    the dispatchgroup moved to viewModel.
    similar movies fetch with getSimilarMovies func.

--- MovieDB/MovieDB/Scene/DetailMovie/DetailMovieViewModel.swift
+++ MovieDB/MovieDB/Scene/DetailMovie/DetailMovieViewModel.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 class DetailMovieViewModel: BaseViewModel {
+    let group = DispatchGroup()
+    
     func getMovieDetailTranslation(movieId: Int, closure: @escaping(DetailMovieModel?) -> Void) -> Void {
         let path = Constant.RequestPathMovie.translationMoviePath(movieId: movieId, language: .tr)
         Network.shared.getRequestV3(urlPath: path) { (result: Result<DetailMovieModel?, Error>) in
@@ -43,14 +45,18 @@ class DetailMovieViewModel: BaseViewModel {
     }
     
     func getActors(movieId: Int, closure: @escaping(MovieActorsModel?) -> Void) -> Void {
+        group.enter()
         let path = Constant.RequestPathMovie.castPath(movieId: movieId)
+        
         Network.shared.getRequestV3(urlPath: path) { (result: Result<MovieActorsModel, Error>) in
             switch result {
             case .success(let data):
                 if data.success ?? true {
                     closure(data)
+                    self.group.leave()
                 } else {
                     closure(nil)
+                    self.group.leave()
                 }
             case .failure(let error):
                 closure(nil)
@@ -60,16 +66,45 @@ class DetailMovieViewModel: BaseViewModel {
     }
     
     func getTrailers(movieId: Int, closure: @escaping(MovieTrailerModel?) -> Void) {
+        group.enter()
         let path = Constant.RequestPathMovie.trailerPath(movieId: movieId)
-        Network.shared.getRequestV3(urlPath: path) { (result: Result<MovieTrailerModel?, Error>) in
+        
+        Network.shared.getRequestV3(urlPath: path) { [weak self] (result: Result<MovieTrailerModel?, Error>) in
+            guard let self else {return}
             switch result {
             case .success(let data):
                 closure(data)
+                self.group.leave()
             case .failure(let error):
                 closure(nil)
+                self.group.leave()
                 print("ERROR MESSAGE: \(error.localizedDescription)")
             }
         }
         
+    }
+    
+    func getSimilarMovie(movieId: Int,_ closure: @escaping(SimilarMovieModel?) -> Void) -> Void {
+        group.enter()
+        let path = Constant.RequestPathMovie.similarMovies(movieId: movieId)
+        
+        Network.shared.getRequestV3(urlPath: path) { [weak self] (result: Result<SimilarMovieModel, Error>) in
+            guard let self else {return}
+            switch result {
+            case .success(let data):
+                closure(data)
+                self.group.leave()
+            case .failure(let error):
+                closure(nil)
+                self.group.leave()
+                print("ERROR MESSAGE: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func closeDispatchGroup(_ closure: @escaping() ->Void) -> Void {
+        group.notify(queue: .main) {
+            closure()
+        }
     }
 }
