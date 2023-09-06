b2a2e4f72a0e5443031b9cb0ca24a7ffbade4803
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-09-06 17:58:59 +0300

    CastDetail MVVM created

---
+++ MovieDB/MovieDB/Scene/CastDetail/CastDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CastDetailViewController.swift
+//  MovieDB
+//
+//  Created by MacBook on 6.09.2023.
+//
+
+import UIKit
+
+class CastDetailViewController: BaseViewController<CastDetailViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel = CastDetailViewModel()
+        
+    }
+
+}
---
+++ MovieDB/MovieDB/Scene/CastDetail/CastDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CastDetailViewModel.swift
+//  MovieDB
+//
+//  Created by MacBook on 6.09.2023.
+//
+
+import Foundation
+
+class CastDetailViewModel: BaseViewModel {
+    
+}
