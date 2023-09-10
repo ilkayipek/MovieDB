46c8d2275e27720f4d0abd406f86d4c80bdfa5fa
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-09-10 20:00:11 +0300

    Update CastDetailViewController.swift
    added property biographyContainerView.
    DetailInfos  set and  avatar image load

--- MovieDB/MovieDB/Scene/CastDetail/CastDetailViewController.swift
+++ MovieDB/MovieDB/Scene/CastDetail/CastDetailViewController.swift
@@ -15,6 +15,7 @@ class CastDetailViewController: BaseViewController<CastDetailViewModel> {
     @IBOutlet weak var birthday: UILabel!
     @IBOutlet weak var placeOfBirth: UILabel!
     @IBOutlet weak var biography: UILabel!
+    @IBOutlet weak var biographyContainerView: CustomContainerUIView!
     @IBOutlet weak var biographyContainterHeightConstraint: NSLayoutConstraint!
     
     var castId: Int?
@@ -22,7 +23,47 @@ class CastDetailViewController: BaseViewController<CastDetailViewModel> {
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel = CastDetailViewModel()
-        
+        setDetailInfos()
     }
-
+    
+    private func setDetailInfos(){
+        if let castId {
+            viewModel?.getPeopleDetail(peopleId: castId) { [weak self] data in
+                guard let self else {return}
+                if let data {
+                    if let avatarPath = data.profilePath {
+                        if let url = Constant.RequestPathMovie.imageUrl(imageSize: .original, path: avatarPath) {
+                            self.avatarImageView.loadImage(url: url, placeHolderImage: nil) { _, _, _, _ in
+                                // animating stop
+                            }
+                        }
+                    }
+                    
+                    if let biography = data.biography {
+                        self.biographyContainerView.isHidden = false
+                        self.biography.text = data.biography
+                    }
+                    
+                    self.nameLabel.text = data.name
+                    self.knownFor.text = data.knownForDepartment
+                    let gender = Gender(rawValue: data.gender ?? 0)
+                    self.gender.text = gender?.description
+                    self.birthday.text = data.birthday
+                    self.placeOfBirth.text = data.placeOfBirth
+                } else {
+                    self.errorStatus()
+                }
+            }
+        } else {
+            errorStatus()
+        }
+    }
+    
+    private func errorStatus() {
+        viewModel?.errorHandlerCompletion?("something went wrong!",.error,.okey) { [weak self] _ in
+            guard let self else {return}
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
 }
