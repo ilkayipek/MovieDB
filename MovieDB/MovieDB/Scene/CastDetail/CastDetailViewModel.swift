af91c297e054aa1acee9b264a72ece2638fee9e1
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-09-10 16:39:59 +0300

    Update CastDetailViewModel.swift
    Bringing the people belonging to the Cast

--- MovieDB/MovieDB/Scene/CastDetail/CastDetailViewModel.swift
+++ MovieDB/MovieDB/Scene/CastDetail/CastDetailViewModel.swift
@@ -9,4 +9,21 @@ import Foundation
 
 class CastDetailViewModel: BaseViewModel {
     
+    func getPeopleDetail(peopleId: Int,_ closure: @escaping(PeopleDetailModel?) -> Void) -> Void {
+        let path = Constant.RequestPathMovie.personDetailPath(personId: peopleId)
+        
+        Network.shared.getRequestV3(urlPath: path) { (result:Result<PeopleDetailModel?, Error>) in
+            switch result {
+            case .success(let data):
+                if let data, data.success ?? true {
+                    closure(data)
+                } else {
+                    closure(nil)
+                }
+            case .failure(let error):
+                closure(nil)
+                print("ERROR MESSAGE: \(error.localizedDescription)")
+            }
+        }
+    }
 }
