f7c464eabba8a21a87a8ef4b0ea55fc6ccd477aa
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-09-10 15:57:26 +0300

    added properties for Cast view and connected.

--- MovieDB/MovieDB/Scene/CastDetail/CastDetailViewController.swift
+++ MovieDB/MovieDB/Scene/CastDetail/CastDetailViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class CastDetailViewController: BaseViewController<CastDetailViewModel> {
-
+    @IBOutlet weak var avatarImageView: CustomUIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var knownFor: UILabel!
+    @IBOutlet weak var gender: UILabel!
+    @IBOutlet weak var birthday: UILabel!
+    @IBOutlet weak var placeOfBirth: UILabel!
+    @IBOutlet weak var biography: UILabel!
+    @IBOutlet weak var biographyContainterHeightConstraint: NSLayoutConstraint!
+    
+    var castId: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel = CastDetailViewModel()
