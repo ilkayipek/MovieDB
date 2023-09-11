cd006ca5612fe7d5050534108de1a6719869b893
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-09-11 21:14:14 +0300

    Update CastDetailViewModel.swift
    func and parameter name 'people' changed with 'person' .
    'GetPersonExternalIds' function created.

--- MovieDB/MovieDB/Scene/CastDetail/CastDetailViewModel.swift
+++ MovieDB/MovieDB/Scene/CastDetail/CastDetailViewModel.swift
@@ -9,10 +9,28 @@ import Foundation
 
 class CastDetailViewModel: BaseViewModel {
     
-    func getPeopleDetail(peopleId: Int,_ closure: @escaping(PeopleDetailModel?) -> Void) -> Void {
-        let path = Constant.RequestPathMovie.personDetailPath(personId: peopleId)
+    func getPersonDetail(personId: Int,_ closure: @escaping(PeopleDetailModel?) -> Void) -> Void {
+        let path = Constant.RequestPathMovie.personDetailPath(personId: personId)
         
         Network.shared.getRequestV3(urlPath: path) { (result:Result<PeopleDetailModel?, Error>) in
+            switch result {
+            case .success(let data):
+                if let data, data.success ?? true {
+                    closure(data)
+                } else {
+                    closure(nil)
+                }
+            case .failure(let error):
+                closure(nil)
+                print("ERROR MESSAGE: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func getPersonExternalIds(personId: Int,_ closure: @escaping(ExternalIDModel?) -> Void) -> Void {
+        let path = Constant.RequestPathMovie.externalIDsPath(personId: personId)
+        
+        Network.shared.getRequestV3(urlPath: path) { (result:Result<ExternalIDModel?, Error>) in
             switch result {
             case .success(let data):
                 if let data, data.success ?? true {
