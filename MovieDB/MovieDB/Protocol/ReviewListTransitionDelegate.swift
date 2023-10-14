a14669e32efb738a9ea7ca887874053bea20e727
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-10-14 15:13:00 +0300

    Create ReviewListTransitionDelegate.swift
    the delegate created for review scene transition

---
+++ MovieDB/MovieDB/Protocol/ReviewListTransitionDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  ReviewListTransitionDelegate.swift
+//  MovieDB
+//
+//  Created by MacBook on 14.10.2023.
+//
+
+protocol ReviewListTransitionDelegate {
+    func reviewListTransition(reviws: ReviewModel)
+}
