2fe1e1e8472ed4eb853bd67fb9038618c9050379
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-10-14 16:43:33 +0300

    ReviewDetailViewController created

---
+++ MovieDB/MovieDB/Scene/ReviewDetail/ReviewDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ReviewDetailViewController.swift
+//  MovieDB
+//
+//  Created by MacBook on 14.10.2023.
+//
+
+import UIKit
+
+class ReviewDetailViewController: UIViewController {
+    
+    @IBOutlet weak var createdAtReviewLabel: UILabel!
+    @IBOutlet weak var contentReviewLabel: UILabel!
+    @IBOutlet weak var authorReviewLabel: UILabel!
+    @IBOutlet weak var authorAvatarImageView: CustomUIImageView!
+    @IBOutlet weak var ratingReviewButton: CustomUIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    
+}
