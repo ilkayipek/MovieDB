1314226271ddb0d2dbb76bd8d0402a3a500e00e8
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-10-14 12:41:03 +0300

    ReviewListController created
    necessary properties added. Table View and reviewList Model array.
    view and property linked.

---
+++ MovieDB/MovieDB/Scene/Reviews/ReviewListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ReviewListViewController.swift
+//  MovieDB
+//
+//  Created by MacBook on 14.10.2023.
+//
+
+import UIKit
+
+class ReviewListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var reviewList = [ReviewModel]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
