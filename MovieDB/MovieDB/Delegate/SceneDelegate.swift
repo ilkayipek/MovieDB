73865cdcd9e5531ef0b740d791e1983260f694bb
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-10-15 19:25:00 +0300

    Update SceneDelegate.swift
    set first scene CustomTabBarController

--- MovieDB/MovieDB/Delegate/SceneDelegate.swift
+++ MovieDB/MovieDB/Delegate/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let rootViewController = HomeViewController()
+        let rootViewController = CustomTabBarController()
         let navigationController = UINavigationController(rootViewController: rootViewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
