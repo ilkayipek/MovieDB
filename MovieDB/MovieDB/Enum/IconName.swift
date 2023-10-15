d9b200c833775d601cc0b38b30a8c5a0fb12fbbb
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-10-15 19:21:30 +0300

    iconeName fix and update
    "iconeName" enum name changed "iconName".
    icone name added for tab bar in iconNameEnum

---
+++ MovieDB/MovieDB/Enum/IconName.swift
@@ -0,0 +1,23 @@
+//
+//  IconeName.swift
+//  MovieDB
+//
+//  Created by MacBook on 16.08.2023.
+//
+
+enum IconName: String {
+    case calendar
+    case clock
+    case arrowChevronDown = "chevron.down"
+    case arrowChevronUp = "chevron.up"
+    case house
+    case houseFill = "house.fill"
+    case movie = "film"
+    case movieFill = "film.fill"
+    case tv
+    case tvFill = "tv.fill"
+    case people = "person.2"
+    case peopleFill = "person.2.fill"
+    case search = "magnifyingglass"
+    case searchFill = "magnifyingglass.circle.fill"
+}
