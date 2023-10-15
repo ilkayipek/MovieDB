3847e221abd5dbe2272a8bc1d099cc420e9b1fa0
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-10-15 17:11:48 +0300

    CustomTabBarController created for home Scene

---
+++ MovieDB/MovieDB/Scene/TabBar/CustomTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarController.swift
+//  MovieDB
+//
+//  Created by MacBook on 15.10.2023.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
