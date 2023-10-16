f11d52de86dcb8c5029b7e56281d88de01900c9b
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-10-16 17:17:57 +0300

    Update HomeViewModel.swift
    popular movies and trend movies movies fetch functions deleted for update home page

--- MovieDB/MovieDB/Scene/Home/HomeViewModel.swift
+++ MovieDB/MovieDB/Scene/Home/HomeViewModel.swift
@@ -6,33 +6,5 @@
 //
 
 class HomeViewModel: BaseViewModel {
-    func getPopularMovies(_ resultClosure: @escaping(PopularMoviesModel?) -> Void) {
-        let path = Constant.RequestPathMovie.popularMoviesPath()
-        Network.shared.getRequestV3(urlPath: path) { (result: Result<PopularMoviesModel, Error>) in
-            switch result {
-            case .success(let data):
-                    if data.success ?? true {
-                        resultClosure(data)
-                    }
-            case .failure(let error):
-                print("ERROR MESSAGE: \(error.localizedDescription)")
-            }
-        }
-    }
     
-    func getTrendMovies(_ resultClosure: @escaping(TrendMoviesModel?) -> Void) {
-        let path = Constant.RequestPathMovie.trendDayMoviesPath()
-        Network.shared.getRequestV3(urlPath: path) { (result: Result<TrendMoviesModel, Error>) in
-            switch result {
-            case .success(let data):
-                if data.success ?? true {
-                    resultClosure(data)
-                } else {
-                    resultClosure(nil)
-                }
-            case .failure(let error):
-                print("ERROR MESSAGE: \(error.localizedDescription)")
-            }
-        }
-    }
 }
