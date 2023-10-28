e7d1a7943b1224ccbed66927785e24666104a42b
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-10-28 12:16:23 +0300

    Update HomeViewModel.swift
    getTrendingAll func created

--- MovieDB/MovieDB/Scene/Home/HomeViewModel.swift
+++ MovieDB/MovieDB/Scene/Home/HomeViewModel.swift
@@ -6,5 +6,21 @@
 //
 
 class HomeViewModel: BaseViewModel {
-    
+    func getTrendingAll(dayOrWeek: DayOrWeek, _ closure: @escaping(MovieAndTVShowModel?) -> Void) {
+        let trendAllPath = Constant.RequestPathMovie.trendingAll(dayOrWeek: dayOrWeek)
+        
+        Network.shared.getRequestV3(urlPath: trendAllPath) { (result: Result<MovieAndTVShowModel?, Error>) in
+            switch result {
+            case .success(let data):
+                if let data = data, !(data.results?.isEmpty ?? true) {
+                    closure(data)
+                } else {
+                    closure(nil)
+                }
+            case .failure(let error):
+                print("MOVİE AND TV MODEL FETCH ERROR: \(error.localizedDescription)")
+                closure(nil)
+            }
+        }
+    }
 }
