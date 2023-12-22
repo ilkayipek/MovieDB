d1c667b77a75fcc079033389408bea0b0121e886
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-12-22 23:50:41 +0300

    Update MoviesViewController.swift
    the moviesViewController.swift created  for movies scene
    tableview configuration was made
    for try, collections listed

--- MovieDB/MovieDB/Scene/Movies/MoviesViewController.swift
+++ MovieDB/MovieDB/Scene/Movies/MoviesViewController.swift
@@ -8,10 +8,49 @@
 import UIKit
 
 class MoviesViewController: BaseViewController<MoviesViewModel> {
+    @IBOutlet weak var collectionListTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel = MoviesViewModel()
+        collectionListTableViewConfigure()
+    }
+    
+    func collectionListTableViewConfigure() {
+        collectionListTableView.delegate = self
+        collectionListTableView.dataSource = self
+        
+        var cellString = String(describing: TrendAllTableViewCell.self)
+        collectionListTableView.register(UINib(nibName: cellString, bundle: nil), forCellReuseIdentifier: cellString)
+        
+        cellString = String(describing: MovieAndTVShowTableViewCell.self)
+        collectionListTableView.register(UINib(nibName: cellString, bundle: nil), forCellReuseIdentifier: cellString)
         
     }
 }
+
+extension MoviesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = collectionListTableView.dequeueReusableCell(withIdentifier: String(describing: TrendAllTableViewCell.self), for: indexPath) as! TrendAllTableViewCell
+            cell.collectionTitle.text = "Trending"
+            return cell
+            
+        case 1,2,3,4:
+            let cell = collectionListTableView.dequeueReusableCell(withIdentifier: String(describing: MovieAndTVShowTableViewCell.self), for: indexPath) as! MovieAndTVShowTableViewCell
+            return cell
+            
+        default:
+            return UITableViewCell()
+        }
+        
+    }
+    
+    
+}
+
