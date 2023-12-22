81e06ca9b955807042cc71af9fe31ace30141ada
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-12-22 23:58:15 +0300

    movieAndTvShowTableViewCell Created

---
+++ MovieDB/MovieDB/Scene/Cells/general/MovieAndTVShowTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MovieAndTVShowTableViewCell.swift
+//  MovieDB
+//
+//  Created by MacBook on 22.12.2023.
+//
+
+import UIKit
+
+class MovieAndTVShowTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
