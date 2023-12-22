e6cbe995a16797189cc06f0337b82c5a9b82f157
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-12-22 23:41:34 +0300

    Create UIColor+Extension.swift
    the extension created for we can using colors in assets

---
+++ MovieDB/MovieDB/Extension/UIColor+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extension.swift
+//  MovieDB
+//
+//  Created by MacBook on 21.12.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let DefaultBackgroundColor = UIColor(named: "BackgroundColor") ?? .white
+}
