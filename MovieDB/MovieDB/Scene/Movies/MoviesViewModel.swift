37fdabcf1d2941c606ea412412c622fa786e9eb9
ilkay-ipek <ilkayy.ipek@gmail.com>
2023-12-27 22:17:40 +0300

    Update MoviesViewModel.swift
    Some functions have been relocated.
    DispatchGroup Group used. all functions wait for get results.

--- MovieDB/MovieDB/Scene/Movies/MoviesViewModel.swift
+++ MovieDB/MovieDB/Scene/Movies/MoviesViewModel.swift
@@ -4,13 +4,42 @@
 //
 //  Created by MacBook on 7.12.2023.
 //
+import Foundation
 
 class MoviesViewModel: BaseViewModel {
+    let dispetchGroup = DispatchGroup()
+    
+    func fetchTrending(dayOrWeek: DayOrWeek, _ closure: @escaping([MovieAndTVShowsModelResult]?) -> Void) -> Void {
+        dispetchGroup.enter()
+        let  path = Constant.RequestPathMovie.trendingMoviesPath(dayOrWeek: dayOrWeek)
+        
+        Network.shared.getRequestV3(urlPath: path) {[weak self] (result:Result<MovieAndTVShowModel, Error>) in
+            guard let self else {return}
+            
+            switch result {
+            case .success(var data):
+                data.collectionTitle = "Trending"
+                if let result = data.results, !result.isEmpty {
+                    closure(result)
+                } else {
+                    closure(nil)
+                }
+            case .failure(let error):
+                closure(nil)
+                print("Trending fatch error:\(error.localizedDescription)")
+            }
+            
+            self.dispetchGroup.leave()
+        }
+    }
     
     func fetchNowPlaying(_ closure: @escaping(MovieAndTVShowModel?)-> Void) -> Void {
+        dispetchGroup.enter()
         let path = Constant.RequestPathMovie.nowPlayingMoviePath()
         
-        Network.shared.getRequestV3(urlPath: path) { (result:Result<MovieAndTVShowModel, Error>) in
+        Network.shared.getRequestV3(urlPath: path) {[weak self] (result:Result<MovieAndTVShowModel, Error>) in
+            guard let self else {return}
+            
             switch result {
             case .success(var data):
                 data.collectionTitle = "Now Playing"
@@ -23,51 +52,18 @@ class MoviesViewModel: BaseViewModel {
                 closure(nil)
                 print("Now playing fatch error:\(error.localizedDescription)")
             }
-        }
-    }
-    
-    func fetchPopular(_ closure: @escaping(MovieAndTVShowModel?)-> Void) -> Void {
-        let path = Constant.RequestPathMovie.popularMoviesPath()
-        
-        Network.shared.getRequestV3(urlPath: path) { (result:Result<MovieAndTVShowModel, Error>) in
-            switch result {
-            case .success(var data):
-                data.collectionTitle = "Popular"
-                if let result = data.results, !result.isEmpty {
-                    closure(data)
-                } else {
-                    closure(nil)
-                }
-            case .failure(let error):
-                closure(nil)
-                print("Popular fatch error:\(error.localizedDescription)")
-            }
-        }
-    }
-    
-    func fetchTopRated(_ closure: @escaping(MovieAndTVShowModel?)-> Void) -> Void {
-        let path = Constant.RequestPathMovie.topRatedMoviePath()
-        
-        Network.shared.getRequestV3(urlPath: path) { (result:Result<MovieAndTVShowModel, Error>) in
-            switch result {
-            case .success(var data):
-                data.collectionTitle = "Top Rated"
-                if let result = data.results, !result.isEmpty {
-                    closure(data)
-                } else {
-                    closure(nil)
-                }
-            case .failure(let error):
-                closure(nil)
-                print("Top rated fatch error:\(error.localizedDescription)")
-            }
+            
+            self.dispetchGroup.leave()
         }
     }
     
     func fetchUpcoming(_ closure: @escaping(MovieAndTVShowModel?)-> Void) -> Void {
+        dispetchGroup.enter()
         let path = Constant.RequestPathMovie.upcomingMoviePath()
         
-        Network.shared.getRequestV3(urlPath: path) { (result:Result<MovieAndTVShowModel, Error>) in
+        Network.shared.getRequestV3(urlPath: path) { [weak self] (result:Result<MovieAndTVShowModel, Error>) in
+            guard let self else {return}
+            
             switch result {
             case .success(var data):
                 data.collectionTitle = "Upcoming"
@@ -80,6 +76,63 @@ class MoviesViewModel: BaseViewModel {
                 closure(nil)
                 print("Upcoming fatch error:\(error.localizedDescription)")
             }
+            
+            self.dispetchGroup.leave()
+        }
+    }
+    
+    func fetchPopular(_ closure: @escaping(MovieAndTVShowModel?)-> Void) -> Void {
+        dispetchGroup.enter()
+        let path = Constant.RequestPathMovie.popularMoviesPath()
+        
+        Network.shared.getRequestV3(urlPath: path) { [weak self] (result:Result<MovieAndTVShowModel, Error>) in
+            guard let self else {return}
+            
+            switch result {
+            case .success(var data):
+                data.collectionTitle = "Popular"
+                if let result = data.results, !result.isEmpty {
+                    closure(data)
+                } else {
+                    closure(nil)
+                }
+            case .failure(let error):
+                closure(nil)
+                print("Popular fatch error:\(error.localizedDescription)")
+            }
+            
+            self.dispetchGroup.leave()
+        }
+    }
+    
+    func fetchTopRated(_ closure: @escaping(MovieAndTVShowModel?)-> Void) -> Void {
+        dispetchGroup.enter()
+        let path = Constant.RequestPathMovie.topRatedMoviePath()
+        
+        Network.shared.getRequestV3(urlPath: path) {[weak self] (result:Result<MovieAndTVShowModel, Error>) in
+            guard let self else {return}
+            
+            switch result {
+            case .success(var data):
+                data.collectionTitle = "Top Rated"
+                if let result = data.results, !result.isEmpty {
+                    closure(data)
+                } else {
+                    closure(nil)
+                }
+            case .failure(let error):
+                closure(nil)
+                print("Top rated fatch error:\(error.localizedDescription)")
+            }
+            
+            self.dispetchGroup.leave()
+        }
+    }
+    
+    //grouping closed
+    func closeDispetchGroup(_ closure: @escaping()->Void) -> Void {
+        dispetchGroup.notify(queue: .main) {
+            closure()
         }
     }
 }
