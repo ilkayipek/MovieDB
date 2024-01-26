cc8c12d16678d1b74f4f7881233867817ff93852
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-01-26 20:47:10 +0300

    AllListCollection MVVM Created
    created to all list media with all button.
    collectionView added.
    was connected collectionView and code

---
+++ MovieDB/MovieDB/Scene/AllListCollection/AllListCollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AllListCollectionViewController.swift
+//  MovieDB
+//
+//  Created by MacBook on 26.01.2024.
+//
+
+import UIKit
+
+class AllListCollectionViewController: BaseViewController<AllListCollectionViewModel> {
+    @IBOutlet weak var allListCollectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ MovieDB/MovieDB/Scene/AllListCollection/AllListCollectionViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AllListCollectionViewModel.swift
+//  MovieDB
+//
+//  Created by MacBook on 26.01.2024.
+//
+
+import Foundation
+
+class AllListCollectionViewModel: BaseViewModel {
+    
+}
