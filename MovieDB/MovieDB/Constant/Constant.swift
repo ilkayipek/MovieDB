843266b3306281febf3b50133c98fb55cfcd0387
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-02-04 14:11:27 +0300

    Update Constant.swift
    defaultLanguage property added

--- MovieDB/MovieDB/Constant/Constant.swift
+++ MovieDB/MovieDB/Constant/Constant.swift
@@ -11,5 +11,6 @@ final class Constant {
     static let shared: Constant = Constant()
     let apiKey: String = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as! String
     let baseUrl: String = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as! String
+    var defaultLanguage = Language.en.rawValue
 }
 
