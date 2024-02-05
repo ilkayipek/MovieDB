b1865ceb1ba7e3f9711a5d3180a6874654773adf
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-02-05 22:48:33 +0300

    Update UIColor+Extension.swift
    LabelColor, ContentTextColor, selectedColor was added.
    according to updated assets

--- MovieDB/MovieDB/Extension/UIColor+Extension.swift
+++ MovieDB/MovieDB/Extension/UIColor+Extension.swift
@@ -9,4 +9,7 @@ import UIKit
 
 extension UIColor {
     static let DefaultBackgroundColor = UIColor(named: "BackgroundColor") ?? .white
+    static let LabelColor = UIColor(named: "LabelColor") ?? .white
+    static let ContentTextColor = UIColor(named: "ContentTextColor") ?? .white
+    static let selectedColor = UIColor(named: "IconColor") ?? .blue
 }
