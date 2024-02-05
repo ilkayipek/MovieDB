6101d7a65f62950c4ac2ceaa543a464161eb9c6c
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-02-05 18:39:18 +0300

    Update AllListCollectionViewModel.swift
    grouping was done.
    
    global page property defined for paging.
    
    'isLoadData' was defined  to see if the next page can be loaded.
    
    fetchAllMoviePageResults function returns list based on selected collection

--- MovieDB/MovieDB/Scene/AllListCollection/AllListCollectionViewModel.swift
+++ MovieDB/MovieDB/Scene/AllListCollection/AllListCollectionViewModel.swift
@@ -9,4 +9,53 @@ import Foundation
 
 class AllListCollectionViewModel: BaseViewModel {
     
+    var page = 1
+    var isLoadData = true
+    let dispatchGroup = DispatchGroup()
+    var resultPages = [MovieAndTVShowsModelResult]()
+    
+    func fetchAllMoviePageResults(collection: MovieCollections, closure: @escaping ([MovieAndTVShowsModelResult]?) ->Void) -> Void{
+        startDispatchGroup()
+        
+//        load next page?
+        if(isLoadData) {
+            let path = Constant.RequestPathMovie.movieCollectionPath(collection: collection, page: page)
+            
+            Network.shared.getRequestV3(urlPath: path) {[weak self] (result:Result<MovieAndTVShowModel?, Error>) in
+                guard let self else {return}
+                
+                switch result {
+                case .failure(let error):
+                    print(error.localizedDescription)
+                case .success(let data):
+                    if let result = data?.results, !result.isEmpty {
+                        self.resultPages = resultPages + result
+                        self.dispatchGroup.leave()
+                        self.closeDispetchGroup()
+                        closure(resultPages)
+                    } else {
+                        isLoadData = false
+                    }
+                    self.page = page + 1
+                }
+        }
+        }
+          
+        
+    }
+    
+    //grouping and animation start func
+    func startDispatchGroup() {
+        dispatchGroup.enter()
+        gradientLoagingTabAnimation?.startAnimations()
+    }
+    
+    
+    //grouping and animation stop func
+    func closeDispetchGroup() -> Void {
+        dispatchGroup.notify(queue: .main) { [weak self] in
+            guard let self else {return}
+            self.gradientLoagingTabAnimation?.stopAnimations()
+        }
+    }
 }
