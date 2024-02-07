62a312d0b8f97a56f31b92f960a6fb165cd72a80
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-02-07 23:30:35 +0300

    Update SearchViewController.swift
    search bar textColor, font, search image, clear button image are set in configuration function.

--- MovieDB/MovieDB/Scene/Search/SearchViewController.swift
+++ MovieDB/MovieDB/Scene/Search/SearchViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class SearchViewController: BaseViewController<SearchViewModel> {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel = SearchViewModel()
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+       searchBarConfiguration()
+    }
+    
+    private func searchBarConfiguration() {
+        searchBar.searchTextField.textColor = .white
+        searchBar.searchTextField.font = UIFont.systemFont(ofSize: 15)
+        
+        if let originalImage = UIImage(systemName: IconName.search.rawValue) {
+            let newSize = CGSize(width: originalImage.size.width * 1.5, height: originalImage.size.height * 1.5)
+            
+            let resizedImage = originalImage.resize(targetSize: newSize).withTintColor(UIColor.LabelColor, renderingMode: .alwaysOriginal)
+            searchBar.setImage(resizedImage, for: .search, state: .normal)
+        }
+        
+        if let originalImage = UIImage().setSystemIcone(.closeFill) {
+            let newSize = CGSize(width: originalImage.size.width * 1.5, height: originalImage.size.height * 1.5)
+            
+            let resizedImage = originalImage.resize(targetSize: newSize).withTintColor(UIColor.LabelColor, renderingMode: .alwaysOriginal)
+            
+            searchBar.setImage(resizedImage, for: .clear, state: .normal)
+        }
+    }
+    
+    
 
 }
