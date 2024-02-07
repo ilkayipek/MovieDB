dc656865434c1e427511b0cd8ea749a8f1fe40d9
ilkay-ipek <ilkayy.ipek@gmail.com>
2024-02-07 22:54:35 +0300

    Create CustomSearchBar.swift
    UISearchbar cusomize extension.

---
+++ MovieDB/MovieDB/Scene/CustomViews/CustomSearchBar.swift
@@ -0,0 +1,42 @@
+//
+//  CustomSearchBar.swift
+//  MovieDB
+//
+//  Created by MacBook on 6.02.2024.
+//
+
+import UIKit
+
+@IBDesignable
+extension UISearchBar {
+
+    @IBInspectable var cornerRadiusValue: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
